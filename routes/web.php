b2429b6f78953e1e000b7b86e8856755ac9f6424
j2cey <jud10parfait@gmail.com>
2020-10-13 14:07:08 +0000

    send mail test route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('send-mail', function () {
+
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+
+    $affectation = \App\Affectation::where('id', 2)->first();
+
+    \Mail::to('j.ngomnze@gabontelecom.ga')->send(new \App\Mail\AffectationNew($affectation));
+
+    dd("Email is Sent.");
+
+});
